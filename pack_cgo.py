11d8c7c862027ee4890ac26a77a74ad12a1f43e5
annaszymanska1205 <annamaria.szymanska@student.uj.edu.pl>
2024-10-02 14:55:29 +0200

    Packing cylinder

--- pack_cgo.py
+++ pack_cgo.py
@@ -1,44 +1,60 @@
-from pymol import cgo
+from pymol import cmd, cgo
 import numpy as np
+import matplotlib.pyplot as plt
 from scipy.spatial.distance import euclidean
+from scipy.optimize import curve_fit
+from copy import copy
 
-def pack_sphere(name : str):
+TRANSPARENCY = 0.4
+SPHERE_QUALITY = 4
+CONE_QUALITY = 100
+COLOR = (1, 1, 1)
+
+def pack_sphere(name : str, margin : float = 1.0):
+    if margin < 0:
+        print("! Warning: negative margin !")
     coord_center = cmd.centerofmass(name)
     model = cmd.get_model(name)
     coords = [atom.coord for atom in model.atom]
     distances = [euclidean(coord_center, coord) for coord in coords]
-    x, y, z, r = *coord_center, np.max(distances) + 1
+    x, y, z, r = *coord_center, np.max(distances) + margin
     sphere = [cgo.SPHERE, x, y, z, r]
     cmd.load_cgo(sphere, f"{name}_sphere")
-    cmd.set("cgo_transparency", 0.6, f"{name}_sphere")
+    cmd.set("cgo_transparency", TRANSPARENCY, f"{name}_sphere")
+    cmd.set("cgo_sphere_quality", SPHERE_QUALITY)
 
 
-def pack_cylinder(name : str):
-    cmd.orient()
+def pack_cylinder(name : str, margin : float = 1.0):
+    cmd.orient(name)
+    if margin < 0:
+        print("! Warning: negative margin !")
+    coord_center = cmd.centerofmass(name)
     model = cmd.get_model(name)
     coords = np.array([atom.coord for atom in model.atom])
-    coord_range = [np.ptp(coords[:, i]) for i in range(3)]
-    idx = np.argmax(coord_range)
-    pos0 = [0, 0, 0]
-    pos1 = [0, 0, 0]
-    pos0[idx] = np.min(coords[:, idx])
-    pos1[idx] = np.max(coords[:, idx])
-    r = 0
-    for i in range(3):
-        if i == idx:
-            continue
-        r = max(r, coord_range[i]/2)
-        pos0[i] = pos1[i] = (np.min(coords[:, i]) + np.max(coords[:, i])) / 2
-    r += 5
-    white = [1, 1, 1]
-    cylinder = [cgo.CONE, *pos0, *pos1, r, r, *white, *white, 0, 1]
+    axis_idx = np.argmax(np.ptp(coords, axis=0))
+    max_dist = 0
+    for coord in coords:
+        coord_center_axis = coord_center
+        coord_center_axis[axis_idx] = coord[axis_idx]
+        max_dist = max(max_dist, euclidean(coord, coord_center_axis))
+    r = max_dist + margin
+    pos0 = copy(coord_center)
+    pos1 = copy(coord_center)
+    pos0[axis_idx] = np.min(coords[:, axis_idx]) + margin
+    pos1[axis_idx] = np.max(coords[:, axis_idx]) + margin
+    cylinder = [cgo.CONE, *pos0, *pos1, r, r, *COLOR, *COLOR, 0, 1]
     cmd.load_cgo(cylinder, f"{name}_cylinder")
-    cmd.set("cgo_transparency", 0.6, f"{name}_cylinder")
+    cmd.set("cgo_transparency", TRANSPARENCY, f"{name}_cylinder")
+    cmd.set("cone_quality", CONE_QUALITY)
 
 
-def pack_cuboid(name : str, cube : bool = False):
+def pack_cuboid(name : str, cube : bool = False, margin : float = 1.0):
+    if margin < 0:
+        print("! Warning: negative margin !")
     pass
 
 
-def pack_cone(name : str, truncated : bool = False):
+def pack_cone(name : str, truncated : bool = False, margin : float = 1.0):
+    if margin < 0:
+        print("! Warning: negative margin !")
     pass
