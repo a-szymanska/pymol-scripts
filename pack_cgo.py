f064145db0cde3669692862684591d4dc6ca72be
annaszymanska1205 <annamaria.szymanska@student.uj.edu.pl>
2024-10-02 20:57:53 +0200

    Packing cone

--- pack_cgo.py
+++ pack_cgo.py
@@ -9,6 +9,8 @@ TRANSPARENCY = 0.4
 SPHERE_QUALITY = 4
 CONE_QUALITY = 100
 COLOR = (1, 1, 1)
+EPSILON = 1e-6
+
 
 def pack_sphere(name : str, margin : float = 1.0):
     if margin < 0:
@@ -25,9 +27,9 @@ def pack_sphere(name : str, margin : float = 1.0):
 
 
 def pack_cylinder(name : str, margin : float = 1.0):
-    cmd.orient(name)
     if margin < 0:
         print("! Warning: negative margin !")
+    cmd.orient(name)
     coord_center = cmd.centerofmass(name)
     model = cmd.get_model(name)
     coords = np.array([atom.coord for atom in model.atom])
@@ -48,13 +50,44 @@ def pack_cylinder(name : str, margin : float = 1.0):
     cmd.set("cone_quality", CONE_QUALITY)
 
 
-def pack_cuboid(name : str, cube : bool = False, margin : float = 1.0):
+def pack_cone(name : str, truncated : bool = False, margin : float = 1.0):
+    cmd.orient(name)
     if margin < 0:
         print("! Warning: negative margin !")
-    pass
+    cmd.orient(name)
+    coord_center = cmd.centerofmass(name)
+    model = cmd.get_model(name)
+    coords = np.array([atom.coord for atom in model.atom])
+    axis_idx = np.argmax(np.ptp(coords, axis=0))
+    distances = []
+    for coord in coords:
+        coord_center_axis = coord_center
+        coord_center_axis[axis_idx] = coord[axis_idx]
+        distances.append(euclidean(coord, coord_center_axis))
+    x_data = coords[:, axis_idx]
+    y_data = distances
+    popt, pcov = curve_fit((lambda x,a,b,c: a*x+b), x_data, y_data)
+    a, b = popt[:2]
+    f = lambda x: a*x + b
+    shift = np.max(y_data - f(x_data)) + EPSILON
+    b += shift
+    r_big = f(np.max(coords[:, axis_idx])) + margin
+    pos_base = copy(coord_center)
+    pos_base[axis_idx] = np.max(coords[:, axis_idx]) + margin
+    pos_top = copy(coord_center)
+    if truncated:
+        r_small = f(np.min(coords[:, axis_idx])) + margin
+        pos_top[axis_idx] = np.min(coords[:, axis_idx]) + margin
+    else:
+        r_small = EPSILON
+        pos_top[axis_idx] = -b/a
+    cone = [cgo.CONE, *pos_top, *pos_base, r_small, r_big, *COLOR, *COLOR, 0, 1]
+    cmd.load_cgo(cone, f"{name}_cone")
+    cmd.set("cgo_transparency", TRANSPARENCY, f"{name}_cone")
+    cmd.set("cone_quality", CONE_QUALITY)
 
 
-def pack_cone(name : str, truncated : bool = False, margin : float = 1.0):
+def pack_cuboid(name : str, cube : bool = False, margin : float = 1.0):
     if margin < 0:
         print("! Warning: negative margin !")
     pass
